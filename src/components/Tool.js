7e9f44ed28d3c4dcc35a10674a0b4706cf5166a1
michudud <pzlc1236@gmail.com>
2023-01-05 14:36:35 +0100

    Change for useContext

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -1,23 +1,28 @@
 import React from "react";
+import { useContext } from "react";
+import DrawingSettingsContext from "./DrawingSettingsContext";
 import MenuIcon from "./MenuIcon";
 
 const Tool = ({
-  activeTool,
   tool_option,
   openOptions,
-  activateTool,
   closeMenuAfterChoose,
   changeIcon,
 }) => {
+  const [toolSettings, setToolSettings] = useContext(DrawingSettingsContext);
+
   return (
     <ul className={`Tool ${openOptions ? "open" : ""}`}>
       {tool_option.map((option, index) =>
-        option !== activeTool ? (
+        option !== toolSettings.activeTool ? (
           <li key={index} className="tool-item">
             <svg
               className="svg-menu-icon"
               onClick={(e) => {
-                activateTool(e);
+                setToolSettings({
+                  ...toolSettings,
+                  activeTool: e.target.parentNode.textContent,
+                });
                 closeMenuAfterChoose();
                 changeIcon(e);
               }}
